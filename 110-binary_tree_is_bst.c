c5ae9995e808ef057e9abc38947fe5803e7cf30f
Addisu-Etana <addisuelectrical@gmail.com>
2024-02-29 06:02:14 -0800

    update

--- 110-binary_tree_is_bst.c
+++ 110-binary_tree_is_bst.c
@@ -1,31 +1,35 @@
 #include "binary_trees.h"
+#include "limits.h"
 
 /**
- * binary_tree_is_bst - Checks if a binary tree is a valid
- * Binary Search Tree.
- * @tree: Pointer to the root node of the tree to check.
+ * is_bst_helper - Checks if a binary tree is a valid binary search tree.
+ * @tree: A pointer to the root node of the tree to check.
+ * @lo: The value of the smallest node visited thus far.
+ * @hi: The value of the largest node visited this far.
  *
- * Return: 1 if tree is a valid BST, 0 otherwise.
+ * Return: If the tree is a valid BST, 1, otherwise, 0.
+ */
+int is_bst_helper(const binary_tree_t *tree, int lo, int hi)
+{
+	if (tree != NULL)
+	{
+		if (tree->n < lo || tree->n > hi)
+			return (0);
+		return (is_bst_helper(tree->left, lo, tree->n - 1) &&
+			is_bst_helper(tree->right, tree->n + 1, hi));
+	}
+	return (1);
+}
+
+/**
+ * binary_tree_is_bst - Checks if a binary tree is a valid binary search tree.
+ * @tree: A pointer to the root node of the tree to check.
+ *
+ * Return: 1 if tree is a valid BST, and 0 otherwise
  */
 int binary_tree_is_bst(const binary_tree_t *tree)
 {
-	static const binary_tree_t *prev;
-
 	if (tree == NULL)
-		return (1);
-
-	/* Checking the left subtree */
-	if (!binary_tree_is_bst(tree->left))
 		return (0);
-
-	/* Checking if current node's value is greater than */
-	/* the value in the previous node (in-order) */
-	if (prev != NULL && tree->n <= prev->n)
-		return (0);
-
-	/* Updating the previous node */
-	prev = tree;
-
-	/* Checking the right subtree */
-	return (binary_tree_is_bst(tree->right));
+	return (is_bst_helper(tree, INT_MIN, INT_MAX));
 }
